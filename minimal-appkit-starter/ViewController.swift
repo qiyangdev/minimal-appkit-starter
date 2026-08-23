4261d2709dcda2e4d9e90cdf5c7d12fdaf687eef
qiyang <wangqiyangx@gmail.com>
2026-08-23 15:48:08 +0800

    Initial Commit

---
+++ minimal-appkit-starter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  minimal-appkit-starter
+//
+//  Created by qiyang on 2026/8/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
